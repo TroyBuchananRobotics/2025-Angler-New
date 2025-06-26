cb9dd8fdd17f5259f00022c1a9a545f59a66c570
Programming <programming9401@gmail.com>
2025-06-26 18:27:21 -0500

    Added the configureMotor function to Climber

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -1,6 +1,23 @@
 package frc.robot.constants;
 
+
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public final class ClimberConstants {
-    public static final int kMotorID = 10;
+    public static final int kMotorID = 18;
     
+    public static final double KS = 0.400390625;
+    public static final double KV = 0.11999999731779099;
+    public static final double KP = 20.0;
+
+    public static final double forwardSoftLimit = 150.0;
+    public static final double reverseSoftLimit = -10.0;
+    
+    public static final double statorCurrentLimit = 50.0;
+    public static final double supplyCurrentLimit = 20.0;
+
+    public static NeutralModeValue neutralMode = NeutralModeValue.Brake;
+    public static InvertedValue invertedMode = InvertedValue.CounterClockwise_Positive;
+
 }
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -7,4 +11,28 @@ import frc.robot.constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
     TalonFX m_motor = new TalonFX(ClimberConstants.kMotorID);
+
+    public Climber(){
+        configureMotor();
+    }
+
+    private void configureMotor(){
+        TalonFXConfiguration motorConfig = new TalonFXConfiguration();
+        motorConfig.withCurrentLimits(new CurrentLimitsConfigs()
+        .withStatorCurrentLimit(ClimberConstants.statorCurrentLimit)
+        .withStatorCurrentLimitEnable(true)
+        .withSupplyCurrentLimit(ClimberConstants.supplyCurrentLimit)
+        .withSupplyCurrentLimitEnable(true));
+        motorConfig.withSlot0(new Slot0Configs()
+        .withKP(ClimberConstants.KP)
+        .withKS(ClimberConstants.KS)
+        .withKV(ClimberConstants.KV));
+        motorConfig.withSoftwareLimitSwitch(new SoftwareLimitSwitchConfigs()
+        .withForwardSoftLimitEnable(true)
+        .withForwardSoftLimitThreshold(ClimberConstants.forwardSoftLimit)
+        .withReverseSoftLimitEnable(true)
+        .withReverseSoftLimitThreshold(ClimberConstants.reverseSoftLimit));
+    }
+
+
 }
