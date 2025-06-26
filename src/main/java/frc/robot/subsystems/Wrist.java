ddd9252a1c7350fc842e71bd7f06ae84a17e938c
Programming <programming9401@gmail.com>
2025-06-25 23:47:14 -0500

    placeholder cancoder added

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
-    TalonFX m_motor = new TalonFX(0);
+    private final TalonFX m_motor = new TalonFX(0);
+    private final CANcoder m_encoder = new CANcoder(1);
     
 }
