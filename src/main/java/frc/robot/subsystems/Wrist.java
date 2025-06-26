b4e8900696c413ebccca6df1972d90ce3168cf9c
Programming <programming9401@gmail.com>
2025-06-25 23:42:18 -0500

    Added Talon FX

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
+    TalonFX m_motor = new TalonFX(0);
     
 }
