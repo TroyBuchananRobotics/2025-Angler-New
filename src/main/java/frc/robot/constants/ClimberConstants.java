0c9b1c461baecd6e88383c44aa8589a02f235395
Programming <programming9401@gmail.com>
2025-06-26 17:40:00 -0500

    Added Motor Declaration and CLimber Constants

---
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public final class ClimberConstants {
+    public static final int kMotorID = 10;
+    
+}
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
-    
+    TalonFX m_motor = new TalonFX(ClimberConstants.kMotorID);
 }
